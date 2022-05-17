bc87a21f55c111e3f45129bddedc2183269d6ae2
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-17 17:28:48 -0400

    installs, migrations

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable("cars", (tbl) => {
+    tbl.increment("id");
+    tbl.string("vin", 13).notNullable().unique();
+    tbl.string("make", 235).notNullable();
+    tbl.string("model", 6).notNullable();
+    tbl.numeric("mileage").notNullable();
+    tbl.string("title");
+    tbl.string("transmission");
+  });
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists("cars");
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,34 @@
 // STRETCH
+const cars = [
+  {
+    vin: "1111111111111",
+    make: "ford",
+    model: "mustang",
+    mielage: 12341,
+    title: "clean",
+    transmission: "manual",
+  },
+  {
+    vin: "1111111111111",
+    make: "volkswagen",
+    model: "passat",
+    mielage: 70000,
+    title: "clean",
+    transmission: "automatic",
+  },
+  {
+    vin: "1111111111111",
+    make: "volkswagen",
+    model: "eurovan",
+    mielage: 12341,
+    title: "clean",
+    transmission: "manual",
+  },
+];
+exports.seed = function (knex) {
+  return knex("cars")
+    .truncate()
+    .then(() => {
+      return knex("cars").insert(cars);
+    });
+};
