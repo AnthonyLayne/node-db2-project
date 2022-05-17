f5c12be3deca1f6b6794054ab7c394081ba8b4f5
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-17 18:04:14 -0400

    fixed errors, added code in server.js

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,11 +1,11 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
   return knex.schema.createTable("cars", (tbl) => {
-    tbl.increment("id");
+    tbl.increments("id");
     tbl.string("vin", 13).notNullable().unique();
     tbl.string("make", 235).notNullable();
     tbl.string("model", 6).notNullable();
-    tbl.numeric("mileage").notNullable();
+    tbl.integer("mileage").notNullable();
     tbl.string("title");
     tbl.string("transmission");
   });
