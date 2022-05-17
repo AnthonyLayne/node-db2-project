78c4f6bbfde45808d8bfb4624231c4a08ef5bb96
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-17 18:32:30 -0400

    added to middleware and router

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,30 @@
+const CAR = require('./cars-model')
+
 const checkCarId = (req, res, next) => {
   // DO YOUR MAGIC
-}
+  try {
+    const cars = await CAR.getById(req.params.id)
+    res.json(cars)
+  } catch (err){
+    next(err)
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,12 +1,29 @@
 // DO YOUR MAGIC
 const express = require("express");
+const CAR = require("./cars-model");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+} = require("./cars-middleware");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json("get all cars");
+  try {
+    const cars = await CAR.getAll();
+    res.json(cars);
+  } catch (err) {
+    next(err);
+  }
 });
-router.get("/:id", async (req, res, next) => {
-  res.json(`get car by id ${req.params.id} `);
+router.get("/:id", checkCarId, async (req, res, next) => {
+  try {
+    const car = await CAR.getById(req.params.id);
+    res.json(car);
+  } catch (err) {
+    next(err);
+  }
 });
 router.post("/", async (req, res, next) => {
   res.json("new post");
