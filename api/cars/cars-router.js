711db2fa507614ab92689dfe010031c07b2c653d
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-17 18:17:27 -0400

    setting up basics for the router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,15 @@
 // DO YOUR MAGIC
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  res.json("get all cars");
+});
+router.get("/:id", async (req, res, next) => {
+  res.json(`get car by id ${req.params.id} `);
+});
+router.post("/", async (req, res, next) => {
+  res.json("new post");
+});
+
+module.exports = router;
